3ca3870bdf9a37188eca137e05814dd731d6a072
Pavlo Yushkovskyi <pavloyus@wizardsdev.com>
2022-01-12 16:26:21 +0200

    サスケは木の葉の里に帰れ
    https://livedoor.blogimg.jp/anigei-mangabox/imgs/7/a/7a86f4f3-s.png

--- src/APIs/groupService.ts
+++ src/APIs/groupService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {apiBase} from "./config";
+import {Group} from "../layouts/teacher/config";
 
 const request = (token: string) => axios.create({
     baseURL: apiBase,
@@ -8,10 +9,18 @@ const request = (token: string) => axios.create({
     }
 });
 
-export const groupApi = (token: string) =>
-    ({
-        getAll()
-        {
-            return request(token).get(`Group/get`);
-        },
-    })
+export const groupApi = (token: string) => ({
+    getAll(){
+        return request(token).get('Group/get');
+    },
+    update(group: Group){
+        return request(token).put('Group/update', group);
+    },
+    create(group: Group){
+        return request(token).post('Group/update', group);
+    },
+    addStudents(groupId: string, userIds: string[]){
+        return request(token).post('Group/addrange', {groupId: groupId, usersIds: userIds});
+    }
+})
+})
